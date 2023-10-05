7b564d7ba8b47cdac42d7a4adbb03b6052fce424
Schwarzepolizei <schwarzepolizei@gmail.com>
2023-10-05 16:43:20 +0400

    Добавлены класс Feed

--- src/tamagotchi/model/actions.py
+++ src/tamagotchi/model/actions.py
@@ -1,15 +1,28 @@
 from abc import ABC, abstractmethod
 
-from creature import Creature
+from creature import Creature, Satiety
 
 
 class Action(ABC):
     name: str
 
-    def __init__(self, origin: Creature, timer: int = 0):
+    def __init__(self, timer: int = None, origin: Creature = None):
         self.origin = origin
         self.timer = timer
 
     @abstractmethod
     def action(self):
         pass
+
+
+class Feed(Action):
+    def __init__(self,
+                 amount: int,
+                 timer: int = None,
+                 origin: Creature = None
+                 ):
+        super().__init__(timer, origin)
+        self.amount = amount
+
+    def action(self):
+        self.origin.params[Satiety] += self.amount
