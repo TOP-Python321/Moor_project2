d4e9442c96da7732fde0a96b2fd5879679aa387d
Schwarzepolizei <schwarzepolizei@gmail.com>
2023-10-06 12:56:51 +0400

    Исправление автотестов

--- test/auto/creature_test.py
+++ test/auto/creature_test.py
@@ -1,8 +1,8 @@
-from model.creature import Maturity, Creature, Kind, Health, Satiety
+from model.creature import Maturity, Creature, Kind, Health, Satiety, cat_kind
 import pytest
 
 
-@pytest.mark.parametrize('kind, name', [(Kind.CAT, "Ovca"), (Kind.CAT, "1234")])
+@pytest.mark.parametrize('kind, name', [(cat_kind, "Ovca"), (cat_kind, "1234")])
 def test_creature_init(kind, name):
     creature = Creature(kind, name)
     assert creature.name == name
@@ -13,7 +13,7 @@ def test_creature_init(kind, name):
 
 @pytest.mark.parametrize('health, hunger', [(9.5, 4.0)])
 def test_creature_update(health, hunger):
-    creature = Creature(Kind.CAT, "Ovca")
+    creature = Creature(cat_kind, "Ovca")
     creature.update()
     assert creature.params[Health].value == health
     assert creature.params[Satiety].value == hunger
@@ -23,7 +23,7 @@ def test_creature_update(health, hunger):
                                                       (Maturity.ADULT, (0, 45), (0, 25)),
                                                       (Maturity.OLD, (0, 35), (0, 20))])
 def test_creature_grow_up(maturity, health, hunger):
-    creature = Creature(Kind.CAT, "Ovca")
+    creature = Creature(cat_kind, "Ovca")
     creature._grow_up(maturity)
     assert creature.params[Health].range == health
     assert creature.params[Satiety].range == hunger
@@ -34,7 +34,7 @@ def test_creature_grow_up(maturity, health, hunger):
                                                          (0, 0, 25, 0),
                                                          (0, 0, 20, 0)])
 def test_hunger_update(value, min_, max_, expected):
-    creature = Creature(Kind.CAT, "Ovca")
+    creature = Creature(cat_kind, "Ovca")
     hunger = Satiety(value, min_, max_, creature)
     hunger.update()
     assert hunger.value == expected
@@ -45,7 +45,7 @@ def test_hunger_update(value, min_, max_, expected):
                                                          (0, 0, 45, 0),
                                                          (0, 0, 35, 0)])
 def test_health_update(value, min_, max_, expected):
-    creature = Creature(Kind.CAT, "Ovca")
+    creature = Creature(cat_kind, "Ovca")
     health = Health(value, min_, max_, creature)
     health.update()
     assert health.value == expected
