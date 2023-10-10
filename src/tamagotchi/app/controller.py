53d0389897f8776de11ad939b26b6de6c312b346
Schwarzepolizei <schwarzepolizei@gmail.com>
2023-10-10 12:17:28 +0400

    Добавление метода start и класса LoadKinds

--- src/tamagotchi/app/controller.py
+++ src/tamagotchi/app/controller.py
@@ -5,14 +5,14 @@ from pathlib import Path
 from sys import path
 
 import model
-
+from model.creature import Creature, State, Kind, cat_kind, dog_kind, mouse_kind
 
 ROOT_DIR = Path(path[0]).parent.parent
 
 
 class App:
     def __init__(self):
-        self.creature: model.Creature = LoadCreature.load() if self._is_live() else MainMenu.choose_kind()
+        self.creature: model.Creature = LoadCreature.load() if self._is_live() else MainMenu.start()
 
     @staticmethod
     def _is_live() -> bool:
@@ -21,10 +21,10 @@ class App:
 
 class LoadCreature:
     default_path: str | Path = ROOT_DIR / 'data/creature.save'
-    game_days_to_real_hours: frac = frac(1, ...)
+    game_days_to_real_hours: frac = frac(1, 2)
 
     @classmethod
-    def save(cls, creature: model.Creature):
+    def save(cls, creature: Creature):
         data = {
             'timestamp': dt.now().timestamp(),
             'kind': creature.kind.name,
@@ -37,11 +37,11 @@ class LoadCreature:
         cls.default_path.write_text(data, encoding='utf-8')
 
     @classmethod
-    def load(cls) -> model.Creature:
+    def load(cls) -> Creature:
         pass
 
     @classmethod
-    def __params_evolution(cls, saved_state: model.State, hours: float) -> model.State:
+    def __params_evolution(cls, saved_state: State, hours: float) -> State:
         """Пересчитывает параметры существа в соответствии с мат.моделью имитации жизни при закрытом приложении (ТЗ п.3в)."""
         hours*cls.game_days_to_real_hours
         ...
@@ -49,9 +49,19 @@ class LoadCreature:
 
 
 class MainMenu:
+
     @staticmethod
-    def choose_kind() -> model.Creature:
-        pass
+    def start():
+        """Запускает GUI с фреймом главного меню."""
+
+    @staticmethod
+    def choose_kind(chosen_kind: Kind) -> Creature:
+        """Создаёт питомца на основе выбранного пользователем вида."""
 
 
+class LoadKinds(list):
+    def __init__(self, *kinds: Kind):
+        super().__init__(kinds)
 
+
+loaded_kinds = LoadKinds(cat_kind, dog_kind, mouse_kind)
